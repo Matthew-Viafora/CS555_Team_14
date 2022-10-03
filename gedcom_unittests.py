9b238288210e45417a0847ee8f1fe058799772b9
Jacob-Roessler <jroess852@gmail.com>
2022-10-03 10:38:01 -0400

    Added more test cases

--- gedcom_unittests.py
+++ gedcom_unittests.py
@@ -1,5 +1,6 @@
 import unittest
 import gedcom_project
+from datetime import date
 
 class TestGedcom(unittest.TestCase):
 
@@ -7,11 +8,23 @@ class TestGedcom(unittest.TestCase):
     def test_age_alive(self):
         test_individual ={ "BIRT": {"DATE": "27 OCT 2000"} }                
         result = gedcom_project.calculateAge(test_individual)
-        self.assertEqual(result, 21)
+        self.assertEqual(result, gedcom_project.timespan(test_individual["BIRT"]["DATE"], date.today()))
+    def test_age_alive2(self):
+        test_individual ={ "BIRT": {"DATE": "1 OCT 2022"} }                
+        result = gedcom_project.calculateAge(test_individual)
+        self.assertEqual(result, gedcom_project.timespan(test_individual["BIRT"]["DATE"], date.today()))
+    def test_age_alive3(self):
+        test_individual ={ "BIRT": {"DATE": "1 OCT 2021"} }                
+        result = gedcom_project.calculateAge(test_individual)
+        self.assertGreaterEqual(result, 1)
     def test_age_deceased(self):
         test_individual ={ "BIRT": {"DATE": "27 OCT 2000"}, "DEAT": {"DATE": "10 JAN 2021"} }                
         result = gedcom_project.calculateAge(test_individual)
         self.assertEqual(result, 20)
+    def test_age_deceased2(self):
+        test_individual ={ "BIRT": {"DATE": "27 OCT 2000"}, "DEAT": {"DATE": "10 JAN 2001"} }                
+        result = gedcom_project.calculateAge(test_individual)
+        self.assertEqual(result, 0)
 
 if __name__ == '__main__':
     unittest.main()
